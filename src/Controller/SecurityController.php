e4caeb3ba154e0e85680559921fe332781de08cf
aherking <a.herking@shopware.com>
2017-12-12 21:10:17 +0100

    Added structure back to base, changed routes for admin login

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -10,7 +10,7 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends Controller
 {
     /**
-     * @Route("/login", name="login")
+     * @Route("/admin/login", name="login")
      */
      public function login(Request $request, AuthenticationUtils $authUtils)
      {
