53331259ed13de4255d80de81808cb0ac174e954
aherking <a.herking@shopware.com>
2017-12-12 18:29:48 +0100

    Changed BlogPostsController to StartController, renaming the Entity

--- src/Controller/StartController.php
+++ src/Controller/StartController.php
@@ -6,10 +6,10 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
-class BlogPostsController extends Controller
+class StartController extends Controller
 {
     /**
-     * @Route("/blog/posts", name="blog_posts")
+     * @Route("/", name="start")
      */
     public function index()
     {
